424c00ae7cd303c3e25461072a4e9972fdefb150
Animesh Raj <rajanimesh64@gmail.com>
2020-08-27 16:13:50 +0530

    Added Status to Cancllation

--- gladiator/src/main/java/com/lti/controller/CancelBookingController.java
+++ gladiator/src/main/java/com/lti/controller/CancelBookingController.java
@@ -16,9 +16,46 @@ public class CancelBookingController {
 	private CancelBookingService cancelBooking;
 	
 	@PostMapping("/cancelBooking")
-	public void cancellation(@RequestBody CancelBookingDto cancelBookingDto) {
+	public Status cancellation(@RequestBody CancelBookingDto cancelBookingDto) {
+		try {
 		int bookingId=cancelBookingDto.getId();
 		cancelBooking.cancelBooking(bookingId);
+		Status status = new Status();
+		status.setStatus(Status.StatusType.SUCCESS);
+		status.setMessage("Cancelled Ticket Successfully!");
+		return status;
+		}
+		catch (Exception e) {
+			Status status = new Status();
+			status.setStatus(Status.StatusType.FAILURE);
+			status.setMessage("Cancellation Failed");
+			return status;
+		}
+	}
+	
+	public static class Status {
+		private StatusType status;
+		private String message;
+		public static enum StatusType{
+			SUCCESS, FAILURE;
+		}
+
+		public StatusType getStatus() {
+			return status;
+		}
+
+		public void setStatus(StatusType status) {
+			this.status = status;
+		}
+		public String getMessage() {
+			return message;
+		}
+
+		public void setMessage(String message) {
+			this.message = message;
+		}
+
+
 	}
 	
 }
